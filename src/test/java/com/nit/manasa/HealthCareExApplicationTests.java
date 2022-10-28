ee9639ca5cb7c0b4771690e3211e32ba0061b524
divakar <divakarrc1991@gmail.com>
2022-10-28 07:19:53 -0400

    first commit;

---
+++ src/test/java/com/nit/manasa/HealthCareExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit.manasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
